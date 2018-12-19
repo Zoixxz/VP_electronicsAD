2fcebbb2141e098dc8e0bb1be32cb642f9933504
rehmannjETHZ <44161339+rehmannjETHZ@users.noreply.github.com>
2018-12-19 15:20:10 +0100

    Add files via upload

--- Python/main.py
+++ Python/main.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
-
-This is a temporary script file.
+Serial Reader Type 1
 """
 import serial
 import time
@@ -10,7 +8,7 @@ import numpy as np
 import sys
 import matplotlib.pyplot as plt
 fanspeed = 0
-ser = serial.Serial('COM4', 9600, timeout=0.1)
+ser = serial.Serial('COM3', 9600, timeout=0.1)
 time.sleep(0.5) #wait for arduino
 Data = np.array([[0, 0, 0, 0]]) # Time, Voltage, RPM, Temperature
 input = '0'
@@ -21,7 +19,7 @@ while input!='42':
     input = input.replace('\r\n', '')
     print('waiting for start')
 
-for i in range(100):
+for i in range(240):
     tempo = np.array([0., 0., 0., 0.])
     j=0
     while j<4:
@@ -38,7 +36,7 @@ for i in range(100):
     
     Data = np.vstack((Data, tempo))
 
-Data = Data[1:-5, :]
+Data = Data[12:-12, :]
 plt.figure()
 plt.plot(Data[1:-1, 0], Data[1:-1, 3], '.')
 plt.xlabel('time [ms]')
@@ -59,6 +57,10 @@ plt.xlabel('time [ms]')
 plt.ylabel('RPM [1/s]')
 plt.savefig('RPM.pdf')
 plt.show()
+
+index = 1
+
+np.savetxt("Experiment%d.txt" %index, Data, delimiter="," , fmt='%f')
 
 
 ser.close()
